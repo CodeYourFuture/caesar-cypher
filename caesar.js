d80e816f1c2126bf73c86e85e4b186c5f939456b
Lorenzo Turrino <neuronefunzionante@gmail.com>
2018-02-24 10:13:42 +0000

    implement read function, create secret message

--- caesar.js
+++ caesar.js
@@ -2,13 +2,17 @@
 The implementation is missing except for the first one as an example.
 At the end of the file, commented out, there is the code to call all functions
 in order and (hopefully) get the right result */
+var fs = require('fs');  // fs is a built-in module of Node, used to read and manipulate files.
 
-function readStringFromFile() {
-  return //something
+function readStringFromFile() {   // implemented for you. Can be improved.
+  var fileName = 'secret_message.txt';  // the name of the file we want to read.
+  var fileContent = fs.readFileSync(fileName);  // read the file as a 'Buffer' object. Sync is for syncronous, avoids using promises or callbacks.
+  var encryptedMessage = fileContent.toString().trim(); // convert to a String, and remove the trailing new line (`\n`)
+  return encryptedMessage
 }
 
 function decryptCharacter(encryptedCharacter) {
-  // implement
+  // implement. Careful about only changing letters and not spaces or symbols :)
   return // a decrypted character
 }
 
